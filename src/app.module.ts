3bdf1dff29e604767f362f9deac867664a4a854f
Manu Puyuelo <puyuelo.m@gmail.com>
2023-09-07 19:46:27 +0200

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ActivitiesModule } from './activities/activities.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ActivitiesModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://admin:ZE6NJJRCYH5MsXLX@cluster0.ppyhv0e.mongodb.net/naboo',
-    ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('CONNECTION_STRING'),
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
