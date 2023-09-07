41f2616239b6c4b4516f36470c420ff8b4abcadf
Manu Puyuelo <puyuelo.m@gmail.com>
2023-09-07 19:31:40 +0200

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Accept'],
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
