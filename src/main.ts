2f348629f4eeed97791458b72224707930c55343
Manu Puyuelo <puyuelo.m@gmail.com>
2023-09-07 19:03:29 +0200

    first and final

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://naboo-front.vercel.app/',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
