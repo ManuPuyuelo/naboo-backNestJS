44877ab46d3a789d5de58bde0a43a97c0bef16e7
Manu Puyuelo <puyuelo.m@gmail.com>
2023-09-07 19:08:00 +0200

    first and final

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
+import * as compression from 'compression';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.use(helmet());
+  app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
+  app.use(compression());
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ extended: true }));
+
+  // Configurer CORS
+  app.enableCors({
+    origin: 'https://naboo-front.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
+
 bootstrap();
