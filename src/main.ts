9235b7519250be98b67ce13d0f15397179ca192d
Manu Puyuelo <puyuelo.m@gmail.com>
2023-09-07 19:23:52 +0200

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://naboo-front.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+    origin: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Accept'],
     credentials: true,
   });
   await app.listen(3000);
